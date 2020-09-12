e1038b917b48aca4a07ad73fc5d1f3884456b581
caolecongminh <caolecongminh1997@gmail.com>
2020-09-12 07:39:56 +0700

    generalizing for using other datasets

--- python/LinearRegression_Visualizing.py
+++ python/LinearRegression_Visualizing.py
@@ -14,14 +14,16 @@ print("""
 def extractDigits(lst):
     return [[el] for el in lst]
 
-# InputFile_Path = input("[ENTER]: Input file path (../image_result/mango/Weight_AreaPixels_A.txt): ")
-# X_name = input("[ENTER]: Name of x coordinate: ")
-# y_name = input("[ENTER]: Name of y coordinate: ")
-InputFile_Path = "../image_result/mango/Weight_AreaPixels_A.txt"
-X_name = "Number of pixels"
-y_name = "Weight"
+InputFile_Path = input("[ENTER]: Input file path (../image_result/mango/Weight_AreaPixels_A.txt): ")
+OuputFolder_Path = input("[ENTER]: Ouput folder path (../image_result/mango/): ")
+FileName_Result = input("[ENTER]: File name result: ")
+X_name = input("[ENTER]: Name of x coordinate: ")
+y_name = input("[ENTER]: Name of y coordinate: ")
+
+print("----------Start-processing----------")
 
 file1 = open(InputFile_Path, "r")
+file2 = open((OuputFolder_Path + FileName_Result + ".txt"), "w")
 
 X_Data_list = []
 y_Data_list = []
@@ -47,18 +49,28 @@ LinearRegression_Model1 = LinearRegression().fit(X_Data_Train_np, y_Data_Train_n
 
 coef = LinearRegression_Model1.coef_
 intercept = LinearRegression_Model1.intercept_
-print("[INFOR] Coefficient: {}".format(coef))
-print("[INFOR] Intercept: {}".format(intercept))
+print("[INFOR]: Coefficient: {}".format(coef))
+print("[INFOR]: Intercept: {}".format(intercept))
+file2.write("[INFOR]: Coefficient: " + str(coef) + "\n")
+file2.write("[INFOR]: Intercept: " + str(intercept) + "\n")
 
-print("Training set score: {:.2f}".format(LinearRegression_Model1.score(X_Data_Train_np, y_Data_Train_np)))
-print("Test set score: {:.2f}".format(LinearRegression_Model1.score(X_Data_Test_np, y_Data_Test_np)))
+training_score = LinearRegression_Model1.score(X_Data_Train_np, y_Data_Train_np)
+test_score = LinearRegression_Model1.score(X_Data_Test_np, y_Data_Test_np)
+print("[INFOR]: Training set score: {:.2f}".format(training_score))
+print("[INFOR]: Test set score: {:.2f}".format(test_score))
+file2.write("[INFOR]: Training set score: " + str(training_score) + "\n")
+file2.write("[INFOR]: Test set score: " + str(test_score) + "\n")
 
 y_pred = LinearRegression_Model1.predict(X_Data_np)
 
 plt.plot(X_Data_np, y_pred, color="red")
 
-plt.show()
-file1.close() 
+plt.savefig(OuputFolder_Path + FileName_Result + ".png")
+plt.savefig(OuputFolder_Path + FileName_Result + ".pdf")
+# plt.show()
+file1.close()
+print("---------------Done---------------")
+
 
 
 
