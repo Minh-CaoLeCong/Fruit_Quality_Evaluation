3414e5b4e9db93e5538d84c9639c83d2459b35b7
caolecongminh <caolecongminh1997@gmail.com>
2020-10-08 18:50:56 +0700

    include opencv

---
+++ C_C++/OpenCV.h
@@ -0,0 +1,11 @@
+#ifndef OPEN_CV_H
+#define OPEN_CV_H
+
+#define MaxIntensity 256
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+#endif //OPEN_CV_H
