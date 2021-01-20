b2e983528e53fb433b94d75271f5a6db3553678d
Minh <caolecongminh1997@gmail.com>
2021-01-21 02:58:23 +0700

    Plot the linear fit, the convergence graph, surface J, contour J

--- matlab/LinearRegression.m
+++ matlab/LinearRegression.m
@@ -10,47 +10,45 @@
 %     plotData.m
 %     gradientDescent.m
 %     computeCost.m
-%     gradientDescentMulti.m
-%     computeCostMulti.m
 %     featureNormalize.m
 %     normalEqn.m
 %
 % x refers to the number of pixels of fruit
 % y refers to the weight of fruit
-%
 
 %% Initialization
 clear ; close all; clc
 fprintf('Linear Regression: Fruit Quality Evaluation \n');
 
-%% ======================= Plotting =======================
-fprintf('Plotting Data ...\n')
+%% ============== Plotting and Feature Normalization ==============
 data = load('Weight_AreaPixels_B_Immaturity_25_50.txt');
-X = data(:, 1); y = data(:, 2);
+X = data(:, 1); 
+y = data(:, 2);
 m = length(y); % number of training examples
 
 % Scale training examples
 % X = X / 100000;
 % y = y / 100;
 
+% Scale features and set them to zero mean
+fprintf('\nNormalizing Features ...\n');
+
 % Mean normalization
 [X mu sigma] = featureNormalize(X);
-[y mu sigma] = featureNormalize(y);
+%[y mu sigma] = featureNormalize(y);
+
+fprintf('Plotting Data ...\n')
 
 % Plot Data
-% Note: You have to complete the code in plotData.m
 plotData(X, y);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
 
 %% =================== Cost and Gradient descent ===================
 
-X = [ones(m, 1) X]; % Add a column of ones to x
+X = [ones(m, 1) X]; % Add intercept term to X
 theta = zeros(2, 1); % initialize fitting parameters
 
 % Some gradient descent settings
-iterations = 150000;
+iterations = 1500;
 alpha = 0.01;
 
 fprintf('\nTesting the cost function ...\n')
@@ -58,36 +56,39 @@ fprintf('\nTesting the cost function ...\n')
 J = computeCost(X, y, theta);
 fprintf('With theta = [0 ; 0]\nCost computed = %f\n', J);
 
-% further testing of the cost function
-J = computeCost(X, y, [0.000000 ; 0.937416]);
-fprintf('\nWith theta = [0.000000 ; 0.937416]\nCost computed = %f\n', J);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
 fprintf('\nRunning Gradient Descent ...\n')
 % run gradient descent
-theta = gradientDescent(X, y, theta, alpha, iterations);
+[theta, J_history] = gradientDescent(X, y, theta, alpha, iterations);
 
 % print theta to screen
 fprintf('Theta found by gradient descent:\n');
 fprintf('%f\n', theta);
 
+% further testing of the cost function
+J = computeCost(X, y, theta);
+fprintf('With theta = %.6f\n', theta);
+fprintf('Cost computed = %.6f\n', J);
+
 % Plot the linear fit
 hold on; % keep previous plot visible
+fprintf('\nPlot the linear fit ...\n')
 plot(X(:,2), X*theta, '-')
 legend('Training data', 'Linear regression')
 hold off % don't overlay any more plots on this figure
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% Plot the convergence graph
+figure;
+fprintf('Plot the convergence graph ...\n')
+plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+xlabel('Number of iterations');
+ylabel('Cost J');
 
-%% ============= Part 4: Visualizing J(theta_0, theta_1) =============
+%% ============= Visualizing J(theta_0, theta_1) =============
 fprintf('Visualizing J(theta_0, theta_1) ...\n')
 
 % Grid over which we will calculate J
-theta0_vals = linspace(-10, 10, 100);
-theta1_vals = linspace(-1, 4, 100);
+theta0_vals = linspace(530, 550, 100);
+theta1_vals = linspace(90, 110, 100);
 
 % initialize J_vals to a matrix of 0's
 J_vals = zeros(length(theta0_vals), length(theta1_vals));
@@ -112,7 +113,7 @@ xlabel('\theta_0'); ylabel('\theta_1');
 % Contour plot
 figure;
 % Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
-contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
+contour(theta0_vals, theta1_vals, J_vals, logspace(1, 3, 100))
 xlabel('\theta_0'); ylabel('\theta_1');
 hold on;
 plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
